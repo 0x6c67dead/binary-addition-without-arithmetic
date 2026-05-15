497c0811bda96c42fae456eee8677b46efbc6e1a
0x6c67dead <guilherme.liratsouza@gmail.com>
2026-05-15 16:09:01 -0300

    ✨ Adição de função para conferir se argumentos = 0 ou 1

--- soma.py
+++ soma.py
@@ -2,11 +2,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("binario_1")
-parser.add_argument("binario_2")
+parser.add_argument("binary_1")
+parser.add_argument("binary_2")
 
 args = parser.parse_args()
 
-print(args.nome)
-print(args.idade)
+binary_array_1 = list(args.binary_1)
+binary_array_2 = list(args.binary_2)
 
+def test_binary_array(binary_array):
+    for value in binary_array:
+        if(not is_binary(value)):
+            print("The both values must be binarys (only 0s and 1s)")
+            print(f"The strange argument: {binary_array}")
+            return 
+
+def is_binary(value):
+    return value in ("0", "1")
+
+def main():
+    test_binary_array(binary_array_1)
+    test_binary_array(binary_array_2)
+
+main()
